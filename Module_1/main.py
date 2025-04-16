93b1ec2ea8f44943b94794f130605a343bda7bb5
kaninn <ilyakaninsrabota@mail.ru>
2025-04-16 16:05:56 +0300

    gui some changes

--- Module_1/main.py
+++ Module_1/main.py
@@ -2,53 +2,53 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from gui_main import Ui_GUI  # Импорт сгенерированного класса
 
-def connect():
-    pass
+# def connect():
+#     pass
 
-def engage():
-    pass
+# def engage():
+#     pass
 
-def disengage():
-    pass
+# def disengage():
+#     pass
 
-def manualCartMode():
-    pass
+# def manualCartMode():
+#     pass
 
-def manuaJointMode():
-    pass
+# def manuaJointMode():
+#     pass
 
-def setJointVelocity():
-    pass
+# def setJointVelocity():
+#     pass
 
-def setCartesianVelocity():
-    pass
+# def setCartesianVelocity():
+#     pass
 
-def moveToStart():
-    pass
+# def moveToStart():
+#     pass
 
-def activateMoveToStart():
-    pass
+# def activateMoveToStart():
+#     pass
 
-def moveToPointL():
-    pass
+# def moveToPointL():
+#     pass
 
-def moveToPointC():
-    pass
+# def moveToPointC():
+#     pass
 
-def moveToPointJ():
-    pass
+# def moveToPointJ():
+#     pass
 
-def play():
-    pass
+# def play():
+#     pass
 
-def pause():
-    pass
+# def pause():
+#     pass
 
-def stop():
-    pass
+# def stop():
+#     pass
 
-def reset():
-    pass
+# def reset():
+#     pass
 
 class MainWindow(QMainWindow):
     def __init__(self):
